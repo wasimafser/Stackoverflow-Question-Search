d654ea40bcba002fce92e1e322dc45e620e61bf9
Wasim Afser <wasim.afser@gmail.com>
2021-05-01 22:00:22 +0530

    serve frontend with django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'web',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+  BASE_DIR / 'web/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
-]
+    path('app/', include('app.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
